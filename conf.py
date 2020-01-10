8456947f8da76b35a0f3541678fe40320641a9b1
Renaud Gaubert <rgaubert@nvidia.com>
2020-01-10 12:11:53 -0800

    Update theme
    Signed-off-by: Renaud Gaubert <rgaubert@nvidia.com>

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'container-wiki'
+project = 'Cloud Native Products'
 copyright = '2020, NVIDIA Corporation'
 author = 'NVIDIA Corporation'
 
@@ -45,9 +45,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "collapse_navigation" : False
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    "custom.css",
+]
+
